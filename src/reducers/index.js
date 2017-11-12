aa26092f9929eda8e3249abada24d875724ad661
Kamil Charubin <kcharubin@hypemobile.pl>
2017-11-12 18:56:15 +0100

    removing unused auth reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import AuthReducer from './AuthReducer';
 import ServersReducer from './ServersReducer';
 import SelectServerReducer from './SelectServerReducer';
 import ServerFormReducer from './ServerFormReducer';
 import FetchedDataReducer from './FetchedDataReducer';
 
 export default combineReducers({
-    auth: AuthReducer,
     servers: ServersReducer,
     selectedOption: SelectServerReducer,
     form: ServerFormReducer,
